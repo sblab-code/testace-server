c99a872e80d7fc3ed3155550e369316cbe480866
Qayyum <qayyumabdul660@gmail.com>
2024-06-25 05:23:33 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({
